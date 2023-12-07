59db53d4649867ac6c43556855e43d349c6364dc
gvazyukova <gvazyukova@mail.ru>
2023-12-07 23:14:43 +0400

    eslint settings

--- task7/rxjs/script1.js
+++ task7/rxjs/script1.js
@@ -1,26 +1,26 @@
-const { range } = rxjs;
+const { range } = rxjs
 
-function isPrime(num) {
-    if (num <= 1) {
-      return false;
-    }
-  
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-  
-    return true;
+function isPrime (num) {
+  if (num <= 1) {
+    return false
   }
-  
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
+
 const numberStream$ = range(0, 100)
-let simpleNumbers = "";
+let simpleNumbers = ''
 
 numberStream$.subscribe(i => {
-    if (isPrime(i)) {
-        simpleNumbers += i + " ";
-      }
+  if (isPrime(i)) {
+    simpleNumbers += i + ' '
+  }
 })
 
 console.log(simpleNumbers)
--- task7/weather/script.js
+++ task7/weather/script.js
@@ -1,72 +1,71 @@
 let city = {
-    lat: 0,
-    lon: 0,
-    name: ""
+  lat: 0,
+  lon: 0,
+  name: ''
 }
 
-function makeWeatherRequest(){
-    return new Promise(function(resolve, reject) {
-        city = JSON.parse(localStorage.getItem("city"))
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + city.lat + '&lon=' + city.lon + '&appid=577b3bd2eec54e5a84a1ae825e746783', true);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-        xhr.send();
+function makeWeatherRequest () {
+  return new Promise(function (resolve, reject) {
+    city = JSON.parse(localStorage.getItem('city'))
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + city.lat + '&lon=' + city.lon + '&appid=577b3bd2eec54e5a84a1ae825e746783', true)
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response)
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        })
+      }
+    }
+    xhr.send()
+  })
+}
+
+function findCoordinates () {
+  const cityName = document.getElementById('city').value
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=577b3bd2eec54e5a84a1ae825e746783')
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length === 0) {
+        alert('Такого города не существует!')
+        document.getElementById('city').value = ''
+      } else {
+        city.lat = data[0].lat
+        city.lon = data[0].lon
+        city.name = data[0].local_names.ru
+        localStorage.setItem('city', JSON.stringify(city))
+        loadWeather()
+      }
     })
 }
 
-function findCoordinates(){
-    let cityName = document.getElementById("city").value; 
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=577b3bd2eec54e5a84a1ae825e746783')
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.length == 0){
-                alert("Такого города не существует!");
-                document.getElementById("city").value = ""
-            }
-            else {
-                city.lat = data[0].lat;
-                city.lon = data[0].lon;
-                city.name = data[0].local_names.ru
-                localStorage.setItem("city", JSON.stringify(city))
-                loadWeather();
-            }
-        })
-}
-
-async function loadWeather(){
-    /*let xhr = new XMLHttpRequest();
+async function loadWeather () {
+  /* let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + city.lat + '&lon=' + city.lon + '&appid=577b3bd2eec54e5a84a1ae825e746783', false);
-    xhr.send();*/
+    xhr.send(); */
 
-    let result = await makeWeatherRequest();
+  const result = await makeWeatherRequest()
 
-        let response = JSON.parse(result);
-        document.getElementById("temperature").innerHTML = response.main.temp + "&#8451;";
-        document.getElementById("pressure").innerHTML = response.main.pressure + " мм.рт.ст";
-        document.getElementById("wind").innerHTML = response.wind.speed + " м/с";
+  const response = JSON.parse(result)
+  document.getElementById('temperature').innerHTML = response.main.temp + '&#8451;'
+  document.getElementById('pressure').innerHTML = response.main.pressure + ' мм.рт.ст'
+  document.getElementById('wind').innerHTML = response.wind.speed + ' м/с'
 
-        loadIcon(response.weather[0].icon);
+  loadIcon(response.weather[0].icon)
 }
 
-async function loadIcon(icon){
-    document.getElementById("weather-icon").src = 'https://openweathermap.org/img/wn/'+icon+'@2x.png'
+async function loadIcon (icon) {
+  document.getElementById('weather-icon').src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
 }
 
-let currentDate = new Date();
-let options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+const currentDate = new Date()
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
 }
-document.getElementById("date").innerHTML = currentDate.toLocaleString("ru", options)
-document.getElementById("city").value = JSON.parse(localStorage.getItem("city")).name
-loadWeather();
+document.getElementById('date').innerHTML = currentDate.toLocaleString('ru', options)
+document.getElementById('city').value = JSON.parse(localStorage.getItem('city')).name
+loadWeather()
